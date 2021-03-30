d5ff5aaa9eb5a06c6eab828f2aa65474fd42b498
OMEN <mrh61037@gmail.com>
2021-03-30 16:43:31 +0300

    update details main Acitivty

--- app/src/main/java/com/nurbk/ps/googleanalyticsassignment/ui/MainActivity.kt
+++ app/src/main/java/com/nurbk/ps/googleanalyticsassignment/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.nurbk.ps.googleanalyticsassignment.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.firestore.FirebaseFirestore
 import com.nurbk.ps.googleanalyticsassignment.BR
 import com.nurbk.ps.googleanalyticsassignment.R
 import com.nurbk.ps.googleanalyticsassignment.adapter.GenericAdapter
@@ -11,10 +14,11 @@ import com.nurbk.ps.googleanalyticsassignment.categories
 import com.nurbk.ps.googleanalyticsassignment.databinding.ActivityMainBinding
 import com.nurbk.ps.googleanalyticsassignment.model.Categories
 
+
 class MainActivity : AppCompatActivity(), GenericAdapter.OnListItemViewClickListener<Categories> {
 
     private lateinit var mBinding: ActivityMainBinding
-
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     private val adapterHome = GenericAdapter(R.layout.item_category, BR.category, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,16 +26,36 @@ class MainActivity : AppCompatActivity(), GenericAdapter.OnListItemViewClickList
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+
+
+
         adapterHome.data = categories
         mBinding.rcData.apply {
             layoutManager = LinearLayoutManager(applicationContext)
             adapter = adapterHome
         }
+
+
+        mBinding.root.setOnClickListener {
+            logDataAnalytics("MainRootDesign", "Click Root Activity")
+        }
+
     }
 
-    override fun onClickItem(itemViewModel: Categories, type: Int) {
+    override fun onClickItem(categories: Categories, type: Int) {
         val intent = Intent(this, DetailsCategoryActivity::class.java)
-        intent.putExtra("data", itemViewModel)
+        intent.putExtra("data", categories)
         startActivity(intent)
+        logDataAnalytics("NameCategories", categories.name)
     }
+
+
+    private fun logDataAnalytics(key: String, data: String) {
+        val bundle = Bundle()
+        bundle.putString(key, data)
+        firebaseAnalytics.logEvent("MainCategoriesActivity", bundle)
+    }
+
 }
